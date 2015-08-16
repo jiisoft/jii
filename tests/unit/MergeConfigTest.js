01c95861dc3aa0f2fcacc6a2fd7aeaa88c8547fe
Kozhin Vladimir <affka@affka.ru>
2015-08-16 09:49:48 +0700

    mergeConfigs() + test, add Environment class

---
+++ tests/unit/MergeConfigTest.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * @namespace Jii
+ * @ignore
+ */
+var Jii = require('../../index');
+
+/**
+ * @class tests.unit.MergeConfigTest
+ * @extends Jii.base.UnitTest
+ */
+var self = Jii.defineClass('tests.unit.MergeConfigTest', {
+
+	__extends: Jii.base.UnitTest,
+
+    mergeTest: function (test) {
+        var o1 = {
+            a: '1',
+            b: [1, 2],
+            c: {
+                d: 'q',
+                e: {
+                    r: 't'
+                }
+            }
+        };
+        var o2 = {
+            b: [3, 4],
+            c: {
+                d: {
+                    y: 'u'
+                },
+                e: '555'
+            }
+        };
+        var o3 = {
+            z: 'e',
+            c: {
+                d: {
+                    h: 'j'
+                }
+            }
+        };
+
+        test.deepEqual(Jii.mergeConfigs(o1, o2, o3), {
+            a: '1',
+            b: [3, 4],
+            c: {
+                d: {
+                    y: 'u',
+                    h: 'j'
+                },
+                e: '555'
+            },
+            z: 'e'
+        });
+        
+        test.done();
+    }
+
+});
+
+module.exports = new self().exports();

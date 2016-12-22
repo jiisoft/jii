769ac8c5c06e4891ebff2c1e9f4bd86fe0071eb6
apanfilov <apanfilov@extpoint.com>
2016-12-22 18:07:10 +0700

    fix DefaultValueValidator.js

--- validators/DefaultValueValidator.js
+++ validators/DefaultValueValidator.js
@@ -6,6 +6,9 @@
 
 var Jii = require('../BaseJii');
 var Validator = require('./Validator');
+var _isArray = require('lodash/isArray');
+var _intersection = require('lodash/intersection');
+var _map = require('lodash/map');
 class DefaultValueValidator extends Validator {
 
     preInit() {
@@ -25,6 +28,20 @@ class DefaultValueValidator extends Validator {
         if (this.isEmpty(object.get(attribute))) {
             object.set(attribute, this.value);
         }
+    }
+
+    validate(object, attributes) {
+        attributes = _isArray(attributes) ? _intersection(this.attributes, attributes) : this.attributes;
+
+        var promises = _map(attributes, attribute => {
+            if (this.skipOnError && object.hasErrors(attribute)) {
+                return;
+            }
+
+            return this.validateAttribute(object, attribute);
+        });
+
+        return Promise.all(promises);
     }
 
 }
